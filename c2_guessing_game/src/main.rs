bd52b97d38687ebbd335755c96ab93d86e742a04
Felix Herrmann <Felix.Herrmann@otto.de>
2025-07-11 11:23:19 +0200

    Guessing game finished

--- c2_guessing_game/src/main.rs
+++ c2_guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 
 use rand::Rng;
 
@@ -6,16 +7,33 @@ fn main() {// Guessing Game in Rust
     println!("Welcome to the Guessing Game!");
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    println!("Please enter your guess between 0 and 100");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please enter your guess between 0 and 100");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
-    
-    println!("The secret number is {secret_number} ")
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal =>
+                {
+                    println!("You win!");
+                    break;
+                },
+        }
+    }
+
 }
+
